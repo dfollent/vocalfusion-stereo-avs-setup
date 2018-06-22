d4b2461c5ba1e91d68fc2f46578cde24f3e3418c
Dan Follent <danielf@xmos.com>
2018-06-22 12:45:18 +0100

    Removed avs-sdk-setup. Updated to avs-device-sdk xmos_v1.6

--- auto_install.sh
+++ auto_install.sh
@@ -2,10 +2,9 @@
 pushd "$( dirname "${BASH_SOURCE[0]}" )" > /dev/null
 SETUP_DIR="$( pwd )"
 RPI_SETUP_DIR=$SETUP_DIR/vocalfusion-rpi-setup
-AVS_SETUP_DIR=$SETUP_DIR/avs-sdk-setup
 
 RPI_SETUP_TAG="v1.1"
-AVS_SETUP_TAG="v1.5"
+AVS_DEVICE_SDK_TAG="xmos_v1.6"
 
 if [ ! -d $RPI_SETUP_DIR ]; then
   git clone -b $RPI_SETUP_TAG git://github.com/xmos/vocalfusion-rpi-setup.git
@@ -23,29 +22,14 @@ else
 
 fi
 
-if [ ! -d $AVS_SETUP_DIR ]; then
-  git clone -b $AVS_SETUP_TAG git://github.com/xmos/avs-sdk-setup.git
-else
-  if ! git -C $AVS_SETUP_DIR diff-index --quiet HEAD -- ; then
-    echo "Changes found in $AVS_SETUP_DIR. Please revert changes, or delete directory, and then rerun."
-    echo "Exiting install script."
-    popd > /dev/null
-    return
-  fi
-
-  echo "Updating Amazon AVS SDK"
-  git -C $AVS_SETUP_DIR fetch > /dev/null
-  git -C $AVS_SETUP_DIR checkout $AVS_SETUP_TAG > /dev/null
-
-fi
-
 # Execute (rather than source) the setup scripts
 echo "Installing VocalFusion Raspberry Pi Setup..."
 $RPI_SETUP_DIR/setup.sh
 
 echo "Installing Amazon AVS SDK..."
-$AVS_SETUP_DIR/setup.sh
+wget -0 https://raw.githubusercontent.com/xmos/avs-device-sdk/$AVS_DEVICE_SDK_TAG/tools/Install/setup.sh
+./setup.sh
 
-echo "Type 'sudo reboot' below to reboot the Raspberry Pi and complete the audio setup."
+echo "Type 'sudo reboot' below to reboot the Raspberry Pi and complete the AVS setup."
 
 popd > /dev/null
